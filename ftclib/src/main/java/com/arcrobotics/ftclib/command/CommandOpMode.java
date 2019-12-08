b9afbb0fc6f3d3d28c1423dcd191c04ec90dc9e7
Peter Chacko <workpraytoday@gmail.com>
2019-12-07 22:44:24 -0600

    added basic command, also added command opmode that needs to get looked at

---
+++ ftclib/src/main/java/com/arcrobotics/ftclib/command/CommandOpMode.java
@@ -0,0 +1,64 @@
+package com.arcrobotics.ftclib.command;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public abstract class CommandOpMode extends LinearOpMode {
+    /**
+     * Initialize all objects, set up subsystems, etc...
+     */
+    public abstract void initialize();
+
+    /**
+     * Run Op Mode. Is called after user presses play button
+     */
+    public abstract void run();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        initialize();
+        waitForStart();
+        run();
+    }
+
+    public void addSequential(Command newCommand) {
+        final Command command = newCommand;
+        command.initialize();
+        final ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(3);
+
+
+        Runnable updateMethod = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    telemetry.addData("Running: ", true);
+                    command.execute();
+                    telemetry.update();
+                } catch(Exception e) {
+                    telemetry.addData("Running: ", false);
+                    telemetry.addData("Exception: ", e);
+
+                    telemetry.update();
+                }
+            }
+        };
+        try {
+            scheduledExecutorService.scheduleAtFixedRate(updateMethod, 0,20, TimeUnit.MILLISECONDS);
+            while(!command.isFinished() && this.opModeIsActive()) {
+                //telemetry.update();
+            }
+            scheduledExecutorService.shutdownNow();
+
+        } catch (Exception e) {
+            command.end();
+            throw e;
+        }
+        command.end();
+    }
+}
+
+
+
