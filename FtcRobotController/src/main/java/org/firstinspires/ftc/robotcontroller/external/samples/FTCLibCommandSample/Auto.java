92141903ac0b8443526393974f375af546d59876
Peter Chacko <workpraytoday@gmail.com>
2019-12-09 22:37:59 -0600

    created a generic motor so I could create a command based sample

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/FTCLibCommandSample/Auto.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.robotcontroller.external.samples.FTCLibCommandSample;
+
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+
+public class Auto extends CommandOpMode {
+
+    DriveSubsystem driveSubsystem;
+    GamepadEx driverGamepad;
+
+    @Override
+    public void initialize() {
+        driverGamepad = new GamepadEx(gamepad1);
+        driveSubsystem = new DriveSubsystem(driverGamepad, hardwareMap, telemetry);
+
+        driveSubsystem.initialize();
+    }
+
+    @Override
+    public void run() {
+        // Drive Forward at half speed for four seconds or until it travels 10 units.
+        addSequential(new DriveForwardCommand(driveSubsystem, 10, 0.5, 4));
+    }
+}
