bd2e58a9f30fd13b8247d95f7f87729cf29aa8d5
Peter Chacko <workpraytoday@gmail.com>
2019-12-11 21:36:42 -0600

    Fix: changed GenericMotorEx to MotorImplEx

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/FTCLibCommandSample/DriveSubsystem.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/FTCLibCommandSample/DriveSubsystem.java
@@ -5,10 +5,9 @@ import com.arcrobotics.ftclib.drivebase.MecanumDrive;
 import com.arcrobotics.ftclib.gamepad.ButtonReader;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
-import com.arcrobotics.ftclib.hardware.motors.GenericMotorEx;
-import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.arcrobotics.ftclib.hardware.motors.MotorImpl;
+import com.arcrobotics.ftclib.hardware.motors.MotorImplEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.util.Hardware;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
@@ -17,9 +16,11 @@ public class DriveSubsystem implements Subsystem {
     GamepadEx driverGamepad;
     Telemetry telemetry;
 
-    public GenericMotorEx backLeftMotor, frontLeftMotor, backRightMotor, frontRightMotor;
+    public MotorImplEx backLeftMotor, frontLeftMotor, backRightMotor, frontRightMotor;
     public MecanumDrive driveTrain;
 
+    final double WHEEL_DIAMETER = 4; // Inches
+    final double PULSES_PER_ROTATION = 280; // NEVEREST 40
     ButtonReader slowDownButton;
 
 
@@ -27,18 +28,18 @@ public class DriveSubsystem implements Subsystem {
         this.driverGamepad = driverGamepad;
         this.telemetry = telemetry;
 
-        backLeftMotor = new GenericMotorEx(hw, "backLeftMotor");
-        frontLeftMotor = new GenericMotorEx(hw, "frontLeftMotor");
-        backRightMotor = new GenericMotorEx(hw, "backRightMotor");
-        frontRightMotor = new GenericMotorEx(hw, "frontRightMotor");
+        backLeftMotor = new MotorImplEx(new MotorImpl(hw, "backLeftMotor", PULSES_PER_ROTATION));
+        frontLeftMotor = new MotorImplEx(new MotorImpl(hw, "frontLeftMotor", PULSES_PER_ROTATION));
+        backRightMotor = new MotorImplEx(new MotorImpl(hw, "backRightMotor", PULSES_PER_ROTATION));
+        frontRightMotor = new MotorImplEx(new MotorImpl(hw, "frontRightMotor", PULSES_PER_ROTATION));
 
         backLeftMotor.setInverted(true);
         frontLeftMotor.setInverted(true);
 
-        backLeftMotor.setDistancePerPulse(0.001);
-        frontLeftMotor.setDistancePerPulse(0.001);
-        backRightMotor.setDistancePerPulse(0.001);
-        frontRightMotor.setDistancePerPulse(0.001);
+        backLeftMotor.setDistancePerPulse((WHEEL_DIAMETER * Math.PI) / (PULSES_PER_ROTATION));
+        frontLeftMotor.setDistancePerPulse((WHEEL_DIAMETER * Math.PI) / (PULSES_PER_ROTATION));
+        backRightMotor.setDistancePerPulse((WHEEL_DIAMETER * Math.PI) / (PULSES_PER_ROTATION));
+        frontRightMotor.setDistancePerPulse((WHEEL_DIAMETER * Math.PI) / (PULSES_PER_ROTATION));
 
 
         driveTrain = new MecanumDrive(frontLeftMotor, backLeftMotor, frontRightMotor, backRightMotor);
@@ -71,8 +72,8 @@ public class DriveSubsystem implements Subsystem {
         else
             maxSpeed = 1;
 
-        driveTrain.driveRobotCentric(driverGamepad.getLeftX() * maxSpeed,
-                driverGamepad.getLeftY()  * maxSpeed, driverGamepad.getRightX() * maxSpeed);
+        driveTrain.driveRobotCentric(driverGamepad.getLeftY()  * maxSpeed,
+                 driverGamepad.getLeftX() * maxSpeed, driverGamepad.getRightX() * maxSpeed);
     }
 
     @Override
