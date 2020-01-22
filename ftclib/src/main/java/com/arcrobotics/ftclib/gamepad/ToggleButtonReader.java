16489b0a9f30ec98902da414d2c1b1e42c20c8a1
Peter Chacko <workpraytoday@gmail.com>
2020-01-21 22:33:40 -0600

    changed gamepad classes to use suppliers

--- ftclib/src/main/java/com/arcrobotics/ftclib/gamepad/ToggleButtonReader.java
+++ ftclib/src/main/java/com/arcrobotics/ftclib/gamepad/ToggleButtonReader.java
@@ -1,5 +1,12 @@
 package com.arcrobotics.ftclib.gamepad;
 
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+import java.util.function.BooleanSupplier;
+
+@RequiresApi(api = Build.VERSION_CODES.N)
 public class ToggleButtonReader extends ButtonReader {
     private boolean currToggleState;
 
@@ -8,7 +15,12 @@ public class ToggleButtonReader extends ButtonReader {
         super(gamepad, button);
 
         currToggleState = false;
+    }
 
+    public ToggleButtonReader(BooleanSupplier buttonValue) {
+        super(buttonValue);
+
+        currToggleState = false;
     }
 
     public boolean getState() {
