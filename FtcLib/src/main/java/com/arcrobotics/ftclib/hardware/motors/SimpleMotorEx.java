74eee085b18c2c103937ba5262aa65e27f3cbcec
echo # <jisenberg1050@gmail.com>
2020-07-01 23:52:33 -0400

    fix: lmao forgot to add these to the git

---
+++ FtcLib/src/main/java/com/arcrobotics/ftclib/hardware/motors/SimpleMotorEx.java
@@ -0,0 +1,58 @@
+package com.arcrobotics.ftclib.hardware.motors;
+
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.arcrobotics.ftclib.controller.wpilibcontroller.SimpleMotorFeedforward;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class SimpleMotorEx extends MotorEx {
+
+    public SimpleMotorEx(SimpleMotor motor, double cpr,
+                         PIDController veloController,
+                         SimpleMotorFeedforward feedforward) {
+
+        super(motor, cpr, veloController, feedforward);
+
+    }
+
+    public SimpleMotorEx(String name, HardwareMap hMap, double cpr,
+                         PIDController veloController,
+                         SimpleMotorFeedforward feedforward) {
+
+        this(new SimpleMotor(name, hMap), cpr, veloController, feedforward);
+
+    }
+
+    public SimpleMotorEx(String name, HardwareMap hMap, double cpr) {
+
+        this(new SimpleMotor(name, hMap), cpr, new PIDController(new double[]{1,0,0}),
+                new SimpleMotorFeedforward(0,0,0));
+
+    }
+
+    public SimpleMotorEx(SimpleMotor motor, double cpr,
+                         PIDController veloController) {
+
+        super(motor, cpr, veloController);
+
+    }
+
+    public SimpleMotorEx(String name, HardwareMap hMap, double cpr,
+                         PIDController veloController) {
+
+        super(new SimpleMotor(name, hMap), cpr, veloController);
+
+    }
+
+    /**
+     * Here we obtain the position of the output shaft in ticks.
+     * We need to cast the motor object as a {@link SimpleMotor}
+     * in order to get this position from the {@link com.qualcomm.robotcore.hardware.DcMotor}.
+     *
+     * @return the current position of the output shaft in ticks
+     */
+    @Override
+    public int getCurrentPosition() {
+        return ((SimpleMotor)motor).getCurrentPosition();
+    }
+
+}
