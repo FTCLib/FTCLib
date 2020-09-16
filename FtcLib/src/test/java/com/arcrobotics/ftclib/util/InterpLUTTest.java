fc532a85cdfa82b9e4fe424c6975903d6190161e
Purav Datta <puravdatta@gmail.com>
2020-09-16 10:48:34 -0500

    Adding interpLUT Unit Test

--- FtcLib/src/test/java/com/arcrobotics/ftclib/util/InterpLUTTest.java
+++ FtcLib/src/test/java/com/arcrobotics/ftclib/util/InterpLUTTest.java
@@ -1,5 +1,34 @@
+package com.arcrobotics.ftclib.util;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class InterpLUTTest {
+InterpLUT lut;
+    @Test
+    public void add() {
 
+    }
+
+    @Test
+    public void createLUT() {
+        lut = new InterpLUT();
+        lut.add(1,1);
+        lut.add(2,2);
+        lut.add(3,3);
+        lut.add(4,4);
+        lut.createLUT();
+    }
+
+    @Test
+    public void get() {
+        InterpLUT lut = new InterpLUT();
+        lut.add(1,1);
+        lut.add(2,2);
+        lut.add(3,3);
+        lut.add(4,4);
+        lut.createLUT();
+        assertEquals(2,lut.get(2),0.000001);
+    }
 }
