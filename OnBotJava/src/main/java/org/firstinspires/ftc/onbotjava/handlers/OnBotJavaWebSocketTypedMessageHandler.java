0e88e1bf665bbdb943bfd693e3179edc02b61b23
Purav Datta <puravdatta@gmail.com>
2020-09-22 10:07:10 -0500

    refactor to move to maven dependency and get rid of openrc

--- OnBotJava/src/main/java/org/firstinspires/ftc/onbotjava/handlers/OnBotJavaWebSocketTypedMessageHandler.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.onbotjava.handlers;
-
-import org.firstinspires.ftc.robotcore.internal.webserver.websockets.WebSocketMessageTypeHandler;
-
-public interface OnBotJavaWebSocketTypedMessageHandler extends WebSocketMessageTypeHandler {
-    String type();
-}
--- RobotCore/src/main/java/com/qualcomm/robotcore/exception/TargetPositionNotSetException.java
+++
@@ -1,7 +0,0 @@
-package com.qualcomm.robotcore.exception;
-
-public class TargetPositionNotSetException extends RuntimeException {
-    public TargetPositionNotSetException() {
-        super("Failed to enable motor. You must set a target position before switching to RUN_TO_POSITION mode");
-    }
-}
