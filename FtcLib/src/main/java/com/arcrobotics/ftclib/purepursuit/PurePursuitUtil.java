a11fcd03178e92df1b80df9eaca3aa35b2dbf211
Volt40 <57955210+Volt40@users.noreply.github.com>
2020-09-24 10:03:16 -0400

    Update PurePursuitUtil.java
    Small patch

--- FtcLib/src/main/java/com/arcrobotics/ftclib/purepursuit/PurePursuitUtil.java
+++ FtcLib/src/main/java/com/arcrobotics/ftclib/purepursuit/PurePursuitUtil.java
@@ -96,7 +96,7 @@ public final class PurePursuitUtil {
 		
 		if (turnOnly)
 			// If turnOnly is true, only return a turn power.
-			return new double[] {0, 0, -angleWrap(ca - ta) / Math.PI};
+			return new double[] {0, 0, angleWrap(ca - -ta) / Math.PI};
 			
 		
 		double absoluteXToPosition = tx - cx;
@@ -112,7 +112,7 @@ public final class PurePursuitUtil {
         
         double powerX = relativeXToPosition / (Math.abs(relativeXToPosition) + Math.abs(relativeYToPosition));
         double powerY = relativeYToPosition / (Math.abs(relativeXToPosition) + Math.abs(relativeYToPosition));
-        double powerTurn = -angleWrap(ca - ta) / Math.PI;
+        double powerTurn = angleWrap(ca - -ta) / Math.PI;
         
         rawMotorPowers = new double[3];
         
