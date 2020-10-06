430addca7ddbad019058181d06547be85421c841
JarnaChao09 <yunyang4928@gmail.com>
2020-10-05 21:01:30 -0400

    change in to input and out to output

--- core/src/main/java/com/arcrobotics/ftclib/kotlin/extensions/util/InterpLUTExt.kt
+++ core/src/main/java/com/arcrobotics/ftclib/kotlin/extensions/util/InterpLUTExt.kt
@@ -2,9 +2,9 @@ package com.arcrobotics.ftclib.kotlin.extensions.util
 
 import com.arcrobotics.ftclib.util.InterpLUT
 
-operator fun InterpLUT.get(`in`: Number): Double =
-        this[`in`.toDouble()]
+operator fun InterpLUT.get(input: Number): Double =
+        this[input.toDouble()]
 
-operator fun InterpLUT.set(`in`: Number, out: Number) {
-    this.add(`in`.toDouble(), out.toDouble())
+operator fun InterpLUT.set(input: Number, output: Number) {
+    this.add(input.toDouble(), output.toDouble())
 }
