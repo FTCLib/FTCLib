9dffd783a947b05dbbe62aa5d5b5ff008ccdd735
Jackson Isenberg <jisenberg1050@gmail.com>
2020-10-08 00:19:56 -0400

    add: use java 1.8 to yml
    Signed-off-by: Jackson Isenberg <jisenberg1050@gmail.com>

--- core/src/test/java/com/arcrobotics/ftclib/command/CommandSchedulerTests.java
+++ core/src/test/java/com/arcrobotics/ftclib/command/CommandSchedulerTests.java
@@ -3,7 +3,6 @@ package com.arcrobotics.ftclib.command;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import com.arcrobotics.ftclib.command.*;
 
 public class CommandSchedulerTests {
 
