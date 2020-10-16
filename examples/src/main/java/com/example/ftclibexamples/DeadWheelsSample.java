67b4dcd7f85d6ddde9ab4461ab2a1cdb2a783f3b
Jackson Isenberg <jisenberg1050@gmail.com>
2020-10-16 15:56:00 -0400

    feat: add comments to dead wheel sample
    Signed-off-by: JIceberg <jisenberg1050@gmail.com>

--- examples/src/main/java/com/example/ftclibexamples/DeadWheelsSample.java
+++ examples/src/main/java/com/example/ftclibexamples/DeadWheelsSample.java
@@ -20,8 +20,18 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 @Disabled
 public class DeadWheelsSample extends LinearOpMode {
 
+    // The lateral distance between the left and right odometers
+    // is called the trackwidth. This is very important for
+    // determining angle for turning approximations
     public static final double TRACKWIDTH = 14.7;
+
+    // Center wheel offset is the distance between the
+    // center of rotation of the robot and the center odometer.
+    // This is to correct for the error that might occur when turning.
+    // A negative offset means the odometer is closer to the back,
+    // while a positive offset means it is closer to the front.
     public static final double CENTER_WHEEL_OFFSET = -2.1;
+
     public static final double WHEEL_DIAMETER = 2.0;
     public static final double TICKS_PER_REV = 8192;
 
