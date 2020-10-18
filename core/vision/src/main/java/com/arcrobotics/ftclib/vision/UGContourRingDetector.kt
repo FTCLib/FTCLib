d8d645b736ca4111de432a901508efbe25c6837b
JarnaChao09 <yunyang4928@gmail.com>
2020-10-18 04:01:22 -0400

    feat-vision: add UGContourRingPipeline and Detector

---
+++ core/vision/src/main/java/com/arcrobotics/ftclib/vision/UGContourRingDetector.kt
@@ -0,0 +1,78 @@
+package com.arcrobotics.ftclib.vision
+
+import com.qualcomm.robotcore.hardware.HardwareMap
+import org.firstinspires.ftc.robotcore.external.Telemetry
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName
+import org.openftc.easyopencv.OpenCvCamera
+import org.openftc.easyopencv.OpenCvCameraFactory
+import org.openftc.easyopencv.OpenCvCameraRotation
+import org.openftc.easyopencv.OpenCvInternalCamera
+
+
+class UGContourRingDetector(
+        private var hardwareMap: HardwareMap,
+        private val telemetry: Telemetry? = null,
+        private var debug: Boolean = false,
+) {
+    lateinit var camera: OpenCvCamera
+    private var isUsingWebcam = false
+    private lateinit var webcamName: String
+    private lateinit var ftcLibPipeline: UGContourRingPipeline
+
+    constructor(hMap: HardwareMap, webcamName: String): this(hMap) {
+        isUsingWebcam = true
+        this.webcamName = webcamName
+    }
+
+    constructor(hMap: HardwareMap, webcamName: String, telemetry: Telemetry, debug: Boolean): this(hMap, telemetry)
+
+    val height: UGContourRingPipeline.Height
+        get() {
+            return UGContourRingPipeline.Height.ZERO
+        }
+
+    fun init() {
+        val cameraMonitorViewId = hardwareMap
+                .appContext
+                .resources
+                .getIdentifier(
+                        "cameraMonitorViewId",
+                        "id",
+                        hardwareMap.appContext.packageName,
+                )
+        camera = if (isUsingWebcam) {
+            OpenCvCameraFactory
+                    .getInstance()
+                    .createWebcam(
+                            hardwareMap.get(
+                                    WebcamName::class.java,
+                                    webcamName
+                            ),
+                            cameraMonitorViewId,
+                    )
+        } else {
+            OpenCvCameraFactory
+                    .getInstance()
+                    .createInternalCamera(
+                            OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId,
+                    )
+        }
+
+        camera.setPipeline(
+                UGContourRingPipeline(
+                        telemetry = telemetry,
+                        debug = debug,
+                ).apply {
+                    ftcLibPipeline = this
+                }
+        )
+
+        camera.openCameraDeviceAsync {
+            camera.startStreaming(320,
+                    240,
+                    OpenCvCameraRotation.UPRIGHT,
+            )
+        }
+    }
+
+}
