3bb90a6996a796a61d4885c3286f812b810a1c23
Jiaming Wang <Jiamingw29@gmail.com>
2020-10-19 18:50:45 -0400

    added comments to vision files

--- core/vision/src/main/java/com/arcrobotics/ftclib/vision/UGRectRingPipeline.java
+++ core/vision/src/main/java/com/arcrobotics/ftclib/vision/UGRectRingPipeline.java
@@ -10,26 +10,42 @@ import org.openftc.easyopencv.OpenCvPipeline;
 
 public class UGRectRingPipeline extends OpenCvPipeline {
 
+    //We declare the mats ontop so we can reuse them later to avoid memory leaks
     private Mat matYCrCb = new Mat();
     private Mat matCbBottom = new Mat();
     private Mat matCbTop = new Mat();
     private Mat topBlock = new Mat();
     private Mat bottomBlock = new Mat();
 
+    //Where the average CB value of the rectangles are stored
     private double topAverage;
     private double bottomAverage;
+
+    //The max difference allowed inside the rectangles
     private int threshold = 15;
+
+    //The position related to the screen
     public static double topRectWidthPercentage = 0.25;
     public static double topRectHeightPercentage = 0.25;
     public static double bottomRectWidthPercentage = 0.25;
     public static double bottomRectHeightPercentage = 0.25;
+
+    //The width and height of the rectangles in terms of pixels
     public static int rectangleWidth = 10;
     public static int rectangleHeight = 10;
 
     @Override
     public Mat processFrame(Mat input) {
+        /**
+         *input which is in RGB is the frame the camera gives
+         *We convert the input frame to the color space matYCrCb
+         *Then we store this converted color space in the mat matYCrCb
+         *For all the color spaces go to
+         *https://docs.opencv.org/3.4/d8/d01/group__imgproc__color__conversions.html
+         */
         Imgproc.cvtColor(input, matYCrCb, Imgproc.COLOR_RGB2YCrCb);
 
+        //The points needed for the rectangles are calculated here
         Rect topRect = new Rect(
                 (int) (matYCrCb.width() * topRectWidthPercentage),
                 (int) (matYCrCb.height() * topRectHeightPercentage),
@@ -44,25 +60,33 @@ public class UGRectRingPipeline extends OpenCvPipeline {
                 rectangleHeight
         );
 
-
+        //The rectangle is drawn into the mat
         drawRectOnToMat(input, topRect, new Scalar(255, 0, 0));
         drawRectOnToMat(input, bottomRect, new Scalar(0, 255, 0));
 
+        //We crop the image so it is only everything inside the rectangles and find the cb value inside of them
         topBlock = matYCrCb.submat(topRect);
         bottomBlock = matYCrCb.submat(bottomRect);
-
         Core.extractChannel(bottomBlock, matCbBottom, 2);
         Core.extractChannel(topBlock, matCbTop, 2);
 
+        //We take the average
         Scalar bottomMean = Core.mean(matCbBottom);
         Scalar topMean = Core.mean(matCbTop);
 
         bottomAverage = bottomMean.val[0];
         topAverage = topMean.val[0];
 
+        //return the mat to be shown onto the screen
         return input;
     }
 
+    /**
+     * Draw the rectangle onto the desired mat
+     * @param mat The mat that the rectangle should be drawn on
+     * @param rect The rectangle
+     * @param color The color the rectangle will be
+     */
     private void drawRectOnToMat(Mat mat, Rect rect, Scalar color) {
         Imgproc.rectangle(mat, rect, color, 1);
     }
