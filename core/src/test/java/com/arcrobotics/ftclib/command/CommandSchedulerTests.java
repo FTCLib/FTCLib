1492c5df0e37435a6135dfd0b5b52a9160474e1b
Jackson Isenberg <jaxonfiles@gatech.edu>
2020-10-20 10:13:36 -0400

    feat: unit testing for button polling and encoder tests
    Signed-off-by: JIceberg <jisenberg1050@gmail.com>

--- core/src/test/java/com/arcrobotics/ftclib/command/CommandSchedulerTests.java
+++ core/src/test/java/com/arcrobotics/ftclib/command/CommandSchedulerTests.java
@@ -1,5 +1,7 @@
 package com.arcrobotics.ftclib.command;
 
+import com.arcrobotics.ftclib.command.button.Trigger;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CommandSchedulerTests {
 
     public static int x = 3;
+    private boolean val = false;
 
     @Test
     public void testAddCommand() {
@@ -85,6 +88,37 @@ public class CommandSchedulerTests {
         CommandScheduler.getInstance().run();
         assertEquals(5, x);
         CommandScheduler.getInstance().reset();
+    }
+
+    @Test
+    public void pollButtons() {
+        x = 3;
+        Robot.enable();
+        Trigger button = new Trigger(this::getValue).whenActive(new CommandBase(){
+            @Override
+            public void execute() {
+                x = 5;
+            }
+
+            @Override
+            public boolean runsWhenDisabled() {
+                return false;
+            }
+        });
+        CommandScheduler.getInstance().run();
+        assertEquals(3, x);
+        updateValue();
+        CommandScheduler.getInstance().run();
+        assertEquals(5, x);
+        CommandScheduler.getInstance().reset();
+    }
+
+    public boolean getValue() {
+        return val;
+    }
+
+    private void updateValue() {
+        val = !val;
     }
 
 }
