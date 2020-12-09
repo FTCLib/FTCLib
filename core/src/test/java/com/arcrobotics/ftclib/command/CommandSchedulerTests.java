7491ca4a6d097727959ca4ad2e0ebef39dd1da4a
JIceberg <jisenberg3@gatech.edu>
2020-12-09 17:00:55 -0500

    update README to include new vision version

--- core/src/test/java/com/arcrobotics/ftclib/command/CommandSchedulerTests.java
+++ core/src/test/java/com/arcrobotics/ftclib/command/CommandSchedulerTests.java
@@ -113,6 +113,26 @@ public class CommandSchedulerTests {
         CommandScheduler.getInstance().reset();
     }
 
+    @Test
+    public void testDefaultCommand() {
+        x = 3;
+        Robot.enable();
+        SubsystemBase p = new SubsystemBase() {
+            @Override
+            public void periodic() {
+                x = 3;
+            }
+        };
+        p.setDefaultCommand(new PerpetualCommand(new InstantCommand(() -> x = 5, p)));
+        CommandScheduler.getInstance().schedule(new InstantCommand(() -> x = 3, p));
+        assertEquals(3, x);
+        CommandScheduler.getInstance().run();
+        assertEquals(5, x);
+        CommandScheduler.getInstance().schedule(new PerpetualCommand(new InstantCommand(() -> x = 3, p)));
+        CommandScheduler.getInstance().run();
+        assertEquals(3, x);
+    }
+
     public boolean getValue() {
         return val;
     }
