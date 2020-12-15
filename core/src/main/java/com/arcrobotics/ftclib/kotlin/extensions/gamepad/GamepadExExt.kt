a04f003726c9071e7d8ca181d7e358e8cc7ef5d2
JarnaChao09 <yunyang4928@gmail.com>
2020-12-15 13:17:38 -0500

    feat: finish adding all infix trigger methods

--- core/src/main/java/com/arcrobotics/ftclib/kotlin/extensions/gamepad/GamepadExExt.kt
+++ core/src/main/java/com/arcrobotics/ftclib/kotlin/extensions/gamepad/GamepadExExt.kt
@@ -11,7 +11,20 @@ infix fun <T: Trigger> T.and(other: T): Trigger = this.and(other)
 infix fun <T: Trigger> T.whenActive(command: () -> Unit): Trigger =
         this.whenActive(InstantCommand(command))
 
-fun <T: Trigger> T.whenActive(vararg requirements: Subsystem, command: () -> Unit) =
-        this.whenActive(InstantCommand(command, *requirements))
+infix fun <T: Trigger> T.whileActiveContinuous(command: () -> Unit): Trigger =
+        this.whileActiveContinuous(InstantCommand(command))
 
-// TODO @JarnaChao DSL maybe
+infix fun <T: Trigger> T.whileActiveOnce(command: () -> Unit): Trigger =
+        this.whileActiveOnce(InstantCommand(command))
+
+infix fun <T: Trigger> T.whenInactive(command: () -> Unit): Trigger =
+        this.whenInactive(InstantCommand(command))
+
+infix fun <T: Trigger> T.toggleWhenActive(command: () -> Unit): Trigger =
+        this.toggleWhenActive(InstantCommand(command))
+
+infix fun <T: Trigger> T.toggleWhenActive(commands: Pair<() -> Unit, () -> Unit>): Trigger =
+        this.toggleWhenActive(InstantCommand(commands.first), InstantCommand(commands.second))
+
+infix fun <T: Trigger> T.cancelWhenActive(command: () -> Unit): Trigger =
+        this.cancelWhenActive(InstantCommand(command))
