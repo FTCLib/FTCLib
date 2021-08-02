4036a6648629a5ff4971e37fa266a36a41ab9538
JIceberg <jisenberg3@gatech.edu>
2021-08-01 21:15:51 -0400

    fix: mecanum sample to be readable and useful

--- examples/src/main/java/com/example/ftclibexamples/MecanumDrivingSample.java
+++ examples/src/main/java/com/example/ftclibexamples/MecanumDrivingSample.java
@@ -1,61 +1,57 @@
 package com.example.ftclibexamples;
 
 import com.arcrobotics.ftclib.drivebase.MecanumDrive;
-import com.arcrobotics.ftclib.geometry.Vector2d;
-import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.hardware.RevIMU;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+@TeleOp
 public class MecanumDrivingSample extends LinearOpMode {
 
-    private MecanumDrive driveTrain;
-
-    private MotorEx fL, fR, bL, bR;
-
-    // the motors used here are goBILDA Yellow Jackets, 435 rpm
+    static final boolean FIELD_CENTRIC = false;
 
     @Override
     public void runOpMode() throws InterruptedException {
-
-        fL = new MotorEx(hardwareMap, "frontLeft", MotorEx.GoBILDA.RPM_435);
-        fR = new MotorEx(hardwareMap, "frontRight", MotorEx.GoBILDA.RPM_435);
-        bL = new MotorEx(hardwareMap, "backLeft", MotorEx.GoBILDA.RPM_435);
-        bR = new MotorEx(hardwareMap, "backRight", MotorEx.GoBILDA.RPM_435);
-
-        driveTrain = new MecanumDrive(
-                fL, fR, bL, bR
+        // constructor takes in frontLeft, frontRight, backLeft, backRight motors
+        // IN THAT ORDER
+        MecanumDrive drive = new MecanumDrive(
+            new Motor(hardwareMap, "frontLeft", Motor.GoBILDA.RPM_435),
+            new Motor(hardwareMap, "frontRight", Motor.GoBILDA.RPM_435),
+            new Motor(hardwareMap, "backLeft", Motor.GoBILDA.RPM_435),
+            new Motor(hardwareMap, "backRight", Motor.GoBILDA.RPM_435)
         );
+
+        RevIMU imu = new RevIMU(hardwareMap);
+        imu.init();
+
+        GamepadEx driverOp = new GamepadEx(gamepad1);
 
         waitForStart();
 
-        driveWithVector(new Vector2d(12, 3));
-        sleep(1000);
-        driveWithVector(new Vector2d(0, 0));
+        while (!isStopRequested()) {
 
-    }
-
-    private void driveWithVector(Vector2d vector) {
-        double[] speeds = normalize(new double[]{vector.getX(), vector.getY()});
-        driveTrain.driveRobotCentric(speeds[0], speeds[1], 0);
-    }
-
-    /**
-     * Normalize the wheel speeds if any value is greater than 1
-     */
-    private double[] normalize(double[] wheelSpeeds) {
-        double maxMagnitude = Math.abs(wheelSpeeds[0]);
-        for (int i = 1; i < wheelSpeeds.length; i++) {
-            double temp = Math.abs(wheelSpeeds[i]);
-            if (maxMagnitude < temp) {
-                maxMagnitude = temp;
+            if (!FIELD_CENTRIC) {
+                // optional fourth parameter for squared inputs
+                drive.driveRobotCentric(
+                        driverOp.getLeftX(),
+                        driverOp.getLeftY(),
+                        driverOp.getRightX(),
+                        false
+                );
+            } else {
+                // optional fifth parameter for squared inputs
+                drive.driveFieldCentric(
+                        driverOp.getLeftX(),
+                        driverOp.getLeftY(),
+                        driverOp.getRightX(),
+                        imu.getRotation2d().getDegrees(),   // gyro value passed in here must be in degrees
+                        false
+                );
             }
-        }
-        if (maxMagnitude > 1.0) {
-            for (int i = 0; i < wheelSpeeds.length; i++) {
-                wheelSpeeds[i] = wheelSpeeds[i] / maxMagnitude;
-            }
-        }
 
-        return wheelSpeeds;
+        }
     }
 
 }
