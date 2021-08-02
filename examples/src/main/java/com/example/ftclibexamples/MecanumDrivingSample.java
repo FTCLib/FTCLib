25ce7ce7418b0e77d29723e5c312f4b8d8b065e2
JIceberg <jisenberg3@gatech.edu>
2021-08-02 01:21:57 -0400

    add: drawings and descriptions throughout sample

--- examples/src/main/java/com/example/ftclibexamples/MecanumDrivingSample.java
+++ examples/src/main/java/com/example/ftclibexamples/MecanumDrivingSample.java
@@ -10,6 +10,10 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp
 public class MecanumDrivingSample extends LinearOpMode {
 
+    // This variable determines whether the following program
+    // uses field-centric or robot-centric driving styles. The
+    // differences between them can be read here in the docs:
+    // https://docs.ftclib.org/ftclib/features/drivebases#control-scheme
     static final boolean FIELD_CENTRIC = false;
 
     @Override
@@ -23,16 +27,55 @@ public class MecanumDrivingSample extends LinearOpMode {
             new Motor(hardwareMap, "backRight", Motor.GoBILDA.RPM_435)
         );
 
+        // This is the built-in IMU in the REV hub.
+        // We're initializing it by its default parameters
+        // and name in the config ('imu'). The orientation
+        // of the hub is important. Below is a model
+        // of the REV Hub and the orientation axes for the IMU.
+        //
+        //                           | Z axis
+        //                           |
+        //     (Motor Port Side)     |   / X axis
+        //                       ____|__/____
+        //          Y axis     / *   | /    /|   (IO Side)
+        //          _________ /______|/    //      I2C
+        //                   /___________ //     Digital
+        //                  |____________|/      Analog
+        //
+        //                 (Servo Port Side)
+        //
+        // (unapologetically stolen from the road-runner-quickstart)
+
         RevIMU imu = new RevIMU(hardwareMap);
         imu.init();
 
+        // the extended gamepad object
         GamepadEx driverOp = new GamepadEx(gamepad1);
 
         waitForStart();
 
         while (!isStopRequested()) {
 
+            // Driving the mecanum base takes 3 joystick parameters: leftX, leftY, rightX.
+            // These are related to the left stick x value, left stick y value, and
+            // right stick x value respectively. These values are passed in to represent the
+            // strafing speed, the forward speed, and the turning speed of the robot frame
+            // respectively from [-1, 1].
+
             if (!FIELD_CENTRIC) {
+
+                // For a robot centric model, the input of (0,1,0) for (leftX, leftY, rightX)
+                // will move the robot in the direction of its current heading. Every movement
+                // is relative to the frame of the robot itself.
+                //
+                //                 (0,1,0)
+                //                   /
+                //                  /
+                //            _____/_____
+                //          /  (0,0,1)   /
+                //         /     ↻     /
+                //        /__________ /
+
                 // optional fourth parameter for squared inputs
                 drive.driveRobotCentric(
                         driverOp.getLeftX(),
@@ -41,6 +84,22 @@ public class MecanumDrivingSample extends LinearOpMode {
                         false
                 );
             } else {
+
+                // Below is a model for how field centric will drive when given the inputs
+                // for (leftX, leftY, rightX). As you can see, for (0,1,0), it will travel forward
+                // regardless of the heading. For (1,0,0) it will strafe right (ref to the 0 heading)
+                // regardless of the heading.
+                //
+                //                   heading
+                //                     /
+                //            (0,1,0) /
+                //               |   /
+                //               |  /
+                //            ___|_/_____
+                //          /           /
+                //         /           / ---------- (1,0,0)
+                //        /__________ /
+
                 // optional fifth parameter for squared inputs
                 drive.driveFieldCentric(
                         driverOp.getLeftX(),
