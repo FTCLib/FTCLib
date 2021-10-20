d22cf4512c4c64d465f2f891ab573c323eb198f7
Jackson Isenberg <jaxonfiles@gatech.edu>
2021-10-20 10:00:51 -0400

    fix: issues with reset in RevIMU (#195)

--- core/src/main/java/com/arcrobotics/ftclib/hardware/RevIMU.java
+++ core/src/main/java/com/arcrobotics/ftclib/hardware/RevIMU.java
@@ -85,10 +85,8 @@ public class RevIMU extends GyroEx {
      * @return Relative heading of the robot
      */
     public double getHeading() {
-        globalHeading = revIMU.getAngularOrientation().firstAngle;
-        relativeHeading = globalHeading + offset;
         // Return yaw
-        return relativeHeading * multiplier;
+        return getAbsoluteHeading() - offset;
     }
 
     /**
@@ -124,7 +122,7 @@ public class RevIMU extends GyroEx {
 
     @Override
     public void reset() {
-        offset = -getHeading();
+        offset += getHeading();
     }
 
     @Override
